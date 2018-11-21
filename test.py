e5b5d8edbcd3f7c17548048ea96464fa83484e29
WinteringAsch <WinteringAsch@users.noreply.github.com>
2018-11-21 16:04:09 +0900

    update test.py
    x position, step 출력, 
    actions.py 위치 수정

--- test.py
+++ test.py
@@ -1,14 +1,21 @@
 from nes_py.wrappers import BinarySpaceToDiscreteSpaceEnv
 import gym_super_mario_bros
+#from gym_super_mario_bros.actions import RIGHT_ONLY
 from src.actions import RIGHT_ONLY
-env = gym_super_mario_bros.make('SuperMarioBros2-v0')
+env = gym_super_mario_bros.make('SuperMarioBros-v0')
 env = BinarySpaceToDiscreteSpaceEnv(env, RIGHT_ONLY)
 
 done = True
-for step in range(2000):
+xpos = 0
+for step in range(1000):
     if done:
         state = env.reset()
     state, reward, done, info = env.step(env.action_space.sample())
+    temp = info.get('x_pos')
+    if temp > xpos:
+        xpos = temp
+        print(xpos, end = '   ')
+        print(step)
     env.render()
 
 env.close()
