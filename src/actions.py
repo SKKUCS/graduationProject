7fc4dc9a6d50534b27a5d2309eb65cf061ec6df4
Silvermist <resetarsi@gmail.com>
2018-11-21 16:01:58 +0900

    src updated

---
+++ src/actions.py
@@ -0,0 +1,7 @@
+RIGHT_ONLY = [
+    ['NOP'],
+    ['right'],
+    ['right', 'A'],
+    ['right', 'B'],
+    ['right', 'A', 'B'],
+]
--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 from nes_py.wrappers import BinarySpaceToDiscreteSpaceEnv
 import gym_super_mario_bros
-from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+from src.actions import RIGHT_ONLY
 env = gym_super_mario_bros.make('SuperMarioBros2-v0')
-env = BinarySpaceToDiscreteSpaceEnv(env, SIMPLE_MOVEMENT)
+env = BinarySpaceToDiscreteSpaceEnv(env, RIGHT_ONLY)
 
 done = True
-for step in range(5000):
+for step in range(2000):
     if done:
         state = env.reset()
     state, reward, done, info = env.step(env.action_space.sample())
